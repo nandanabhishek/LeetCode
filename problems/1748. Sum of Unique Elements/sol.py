5ecc23e9a7c46045b801c49fa2fb993cbf8ecc23
Abhishek Nandan <abhisheknandan00@gmail.com>
2021-05-19 22:19:47 +0530

    Update sol.py

--- problems/1748. Sum of Unique Elements/sol.py
+++ problems/1748. Sum of Unique Elements/sol.py
@@ -7,5 +7,7 @@ class Solution:
         return sum(uniq)
     
     
-    # nums.count(num) calculates the number of times num is present in the list nums !!!
+    # nums.count(num) calculates the number of times num is present in the list-nums !!!
+    # we are simply appending those num which has occured only one time in the list-nums
+    # finally we sum those elements which are present only once in the list-nums
             
