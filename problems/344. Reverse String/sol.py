71551329f0a7dc91c84833eb5832a7904a85b98b
Abhishek Nandan <abhisheknandan00@gmail.com>
2021-05-21 20:41:38 +0530

    Create sol.py

---
+++ problems/344. Reverse String/sol.py
@@ -0,0 +1,10 @@
+# Approach - 1: What I thought
+
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        s.reverse()
+        
