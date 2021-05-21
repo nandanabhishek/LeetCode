1a0637c70ad1c7d1d4367f735016b406e47c8d8e
Abhishek Nandan <abhisheknandan00@gmail.com>
2021-05-22 00:04:52 +0530

    Create sol.py

---
+++ problems/58. Length of Last Word/sol.py
@@ -0,0 +1,8 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        wordlist = s.split()
+        if wordlist:
+            return len(wordlist[-1])
+        return 0
+        
+        
