d89695a5090f72e9251329edd7fc92b6ee153e9d
Abhishek Nandan <abhisheknandan00@gmail.com>
2021-05-24 09:02:56 +0530

    Create sol.py

---
+++ problems/144. Binary Tree Preorder Traversal/sol.py
@@ -0,0 +1,67 @@
+# Approach - 1 : Recursive
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        # base case
+        if not root:
+            return []
+        else:
+            return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
+                
+        
+        
+        
+# Approach - 2 : Recursive
+
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        # preorder: root->left->right
+        ans = []
+        def dfs(node):
+            if not node:
+                return
+            ans.append(node.val)
+            dfs(node.left)
+            dfs(node.right)
+        dfs(root)
+        return ans
+      
+      
+      
+      
+      
+      
+      
+      
+# Approach - 3 : Iterative
+
+
+
+
+def preorderTraversal(self, root):
+    stack, res = [root], []
+    while stack:
+        node = stack.pop()
+        if node:
+            res.append(node.val)
+            stack.append(node.right)
+            stack.append(node.left)
+    return res
+
+
+
