4889c556e6d951f53d1cfc0ccdecb7e46d8b70eb
Abhishek Nandan <abhisheknandan00@gmail.com>
2021-05-25 10:12:19 +0530

    Create sol.py

---
+++ problems/589. N-ary Tree Preorder Traversal/sol.py
@@ -0,0 +1,33 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        
+        output =[]
+        
+        # perform dfs on the root and get the output stack
+        self.dfs(root, output)
+        
+        # return the output of all the nodes.
+        return output
+    
+    
+    def dfs(self, root, output):
+        
+        # If root is none return 
+        if root is None:
+            return
+        
+        # for preorder we first add the root val
+        output.append(root.val)
+        
+        # Then add all the children to the output
+        for child in root.children:
+            self.dfs(child, output)
+        
