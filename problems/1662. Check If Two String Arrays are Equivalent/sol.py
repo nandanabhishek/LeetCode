74b4ccf93ac93d2248419fea8b16e48370665457
Abhishek Nandan <abhisheknandan00@gmail.com>
2021-05-28 20:32:42 +0530

    Create sol.py

---
+++ problems/1662. Check If Two String Arrays are Equivalent/sol.py
@@ -0,0 +1,16 @@
+# Approach - 1 : What I thought
+
+
+
+class Solution:
+    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        
+        str1 = ''
+        for i in word1:
+            str1 += i
+            
+        str2=''
+        for j in word2:
+            str2 += j
+            
+        return str1==str2
