f5f7bd5041eaeaf9d76b8436e3c5579df294ef43
Abhishek Nandan <abhisheknandan00@gmail.com>
2021-06-01 19:12:48 +0530

    Create sol.py

---
+++ problems/81. Search in Rotated Sorted Array II/sol.py
@@ -0,0 +1,10 @@
+# general sol and easy but less efficient - Linear Search  : Time Complexity - O(n)
+
+
+
+# Better sol and good solution  - Binary Search  : Time complexity - O(log n) !!! 
+# Learn how to use Binary Search in Sorted and Rotated array
+
+
+
+
