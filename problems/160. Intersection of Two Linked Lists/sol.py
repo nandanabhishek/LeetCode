ad35712cc2fa44c8e2b6d957e54d875c6d805da7
Abhishek Nandan <abhisheknandan00@gmail.com>
2021-06-12 00:49:57 +0530

    Update sol.py

--- problems/160. Intersection of Two Linked Lists/sol.py
+++ problems/160. Intersection of Two Linked Lists/sol.py
@@ -1,3 +1,39 @@
+# Approach - 1 : best one
+
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        
+        if not headA or not headB:
+            return None
+        
+        currA, currB = headA, headB
+        
+        while currA != currB:
+            currB = headA if currB is None else currB.next
+            currA = headB if currA is None else currA.next
+            
+        return currA
+            
+            
+            
+        
+
+
+
+
+# Approach - 2:
+
+
+
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
